62c88903259dd0cd2ba8722a9cb6adec481a4ba6
Code1sArt <137850119+Code1sArt@users.noreply.github.com>
2023-07-01 21:39:42 +0700

    แก้ไข theme pt.1

--- Schoo_Manage/resources/views/template/template.blade.php
+++ Schoo_Manage/resources/views/template/template.blade.php
@@ -18,8 +18,8 @@
 <!-- Page Loader -->
 <div class="page-loader-wrapper">
     <div class="loader">
-        <div class="m-t-30"><img src="assets/images/logo.svg" width="48" height="48" alt="Alpino"></div>
-        <p>Please wait...</p>        
+        <div class="m-t-30"><img src="assets/images/logo.png" width="48" height="48" alt="Alpino"></div>
+        <p>กรุณารอสักครู่...</p>        
     </div>
 </div>
 <div class="overlay_menu">
@@ -58,7 +58,7 @@
         <li>
             <div class="navbar-header">
                 <a href="javascript:void(0);" class="h-bars"></a>
-                <a class="navbar-brand" href="index.html"><img src="assets/images/logo-black.svg" width="35" alt="Alpino"><span class="m-l-10">Alpino</span></a>
+                <a class="navbar-brand" href="/"><img src="assets/images/logo.png" width="35" alt="Alpino"><span class="m-l-10">Smart TR68</span></a>
             </div>
         </li>
         <li class="search_bar">
@@ -206,7 +206,7 @@
         <div class="row">
             <div class="col-md-12">                
                 <ul class="h-menu">
-                    <li><a href="index.html"><i class="zmdi zmdi-home"></i></a></li>
+                    <li><a href="/"><i class="zmdi zmdi-home"></i></a></li>
                     <li><a href="javascript:void(0)">Apps</a>
                         <ul class="sub-menu">                                   
                             <li><a href="mail-inbox.html">Inbox</a></li>
@@ -352,6 +352,7 @@
     </div>
 </div>
 
+<!-- Part Setting Menu Aside -->
 <aside class="right_menu">
     <div id="rightsidebar" class="right-sidebar">
         <ul class="nav nav-tabs">
@@ -527,44 +528,7 @@
 
 <!-- Main Content -->
 <section class="content">
-    <div class="container">
-        <div class="block-header">
-            <div class="row clearfix">
-                <div class="col-lg-5 col-md-5 col-sm-12">
-                    <h2>Stater Page</h2>                    
-                </div>            
-                <div class="col-lg-7 col-md-7 col-sm-12">
-                    <ul class="breadcrumb float-md-right padding-0">
-                        <li class="breadcrumb-item"><a href="index.html"><i class="zmdi zmdi-home"></i></a></li>
-                        <li class="breadcrumb-item"><a href="javascript:void(0);">Pages</a></li>
-                        <li class="breadcrumb-item active">Stater Page</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="row clearfix">
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="header">
-                        <h2><strong>Blank</strong> Page</h2>                        
-                        <ul class="header-dropdown">
-                            <li class="dropdown"> <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"> <i class="zmdi zmdi-more"></i> </a>
-                                <ul class="dropdown-menu dropdown-menu-right">
-                                    <li><a href="javascript:void(0);">Action</a></li>
-                                    <li><a href="javascript:void(0);">Another action</a></li>
-                                    <li><a href="javascript:void(0);">Something else</a></li>
-									<li><a href="javascript:void(0);" class="boxs-close">Delete</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="body">
-                        <h4 class="margin-0">Stater page</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @yield('main_content')
 </section>
 <!-- Jquery Core Js --> 
 <script src="assets/bundles/libscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js --> 
